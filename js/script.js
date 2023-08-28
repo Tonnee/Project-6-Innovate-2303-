c7500dbc9afbc07e596aa6c69100c1a19de2afee
Sabiha Afrin <satonnee@gmail.com>
2023-08-28 16:16:43 +0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,64 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                dm: ["DM Sans"],
+                chivo: ["Chivo"],
+            },
+            fontSize: {
+                56: "56px",
+                46: "46px",
+                44: "44px",
+                22: "22px",
+            },
+            colors: {
+                primary: "#3639a4",
+                secondary: "#43e7df",
+                third: "#645e76",
+                highlight: "#fdb400",
+                tag: "#FF7364",
+                line: "#DCE7FE",
+                'sky-blue': "#2290F5",
+                "green-dark": "#28AE61",
+                "icon-red-light": "#FAEAE8",
+                "icon-blue-light": "#E8F2FA",
+                "icon-green-light": "#E8FAF0",
+            },
+            spacing: {
+                26: "26px",
+                30: "30px",
+                50: "50px",
+                100: "100px",
+                116: "116px",
+                140: "140px",
+                195: "195px",
+                200: "200px",
+                291: "291px",
+                504: "504px",
+                530: "530px",
+                558: "558px",
+            },
+            maxWidth: {
+                container: "1410px",
+                796: "796px",
+                530: "530px",
+                504: "504px",
+                467: "467px",
+                300: "300px",
+            },
+            minWidth: {
+                300: "300px",
+                480: "480px",
+            },
+            borderRadius: {
+                20: "20px",
+            },
+            width: {
+                92: "92px",
+            },
+            height: {
+                92: "92px",
+            },
+        },
+    },
+};
